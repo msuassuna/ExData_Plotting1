fd3f9ac90d853846b83bb1be644ef2991639bf64
Marcus <msuassuna@gmail.com>
2017-01-20 09:05:55 +0100

    Loading plot1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,11 @@
+rm(list = ls())
 setwd("~/Specialization_DataScience/4_Exploratory/Week1/Assignment/ExData_Plotting1")
-
-source("ReadData.R")
+ElectCons <- read.table("household_power_consumption.txt",sep = ";",
+                        skip = 66637,
+                        nrows = 2880)
+names(ElectCons) <- names(read.table("household_power_consumption.txt",sep = ";",
+                                     nrows = 1,
+                                     header = TRUE))
 
 # Plot1
 png(file="plot1.png", width=480, height=480)
